c5eef23eb13bc1463f5a4e5816d88f369d4c4270
Tom Gurion <nagasaki45@gmail.com>
2015-05-07 22:52:53 +0300

    Add Google Analytics

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = "galilee-bedouin-camplodge"
-#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS = 'UA-62733922-1'
