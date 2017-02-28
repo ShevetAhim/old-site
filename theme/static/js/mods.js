b3dba43508bf603854d4e0d52a674f9ceb5617e2
Tom Gurion <nagasaki45@gmail.com>
2017-02-28 22:36:08 +0000

    Remove unused js code (setting the height of some items)

--- theme/static/js/mods.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    // Set items in rows to the same height
-    items = $('.row-item');
-    maxHeight = Math.max.apply(
-    Math, items.map(function() {
-        return $(this).height();
-    }).get());
-    items.height(maxHeight);
-});
