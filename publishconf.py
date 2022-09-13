cd2b45008291820f1d10c975d2516e2b2bdf9128
Tom Gurion <nagasaki45@gmail.com>
2022-09-13 21:58:25 +0100

    Fixing deployment to GH pages

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.galilee-bedouin-camplodge.com'
+SITEURL = 'https://galilee-bedouin-camplodge.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
