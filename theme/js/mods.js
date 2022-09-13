84bd3eb8cb67ac0321c0bfa7beffefadaf9be93f
Nagasaki45 <nagasaki45@gmail.com>
2022-09-13 20:45:41 +0000

    Deploying to gh-pages from @ ShevetAhim/old-site@79c2ce17f6a87f177ed240ab61809af5db2fdc65 🚀

---
+++ theme/js/mods.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#carousel').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        adaptiveHeight: true,
+        cssEase: 'linear'
+    });
+});
